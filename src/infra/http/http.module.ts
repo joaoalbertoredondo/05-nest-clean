78ac562775e4c6ba59c58a4269cd9c62edfc021d
João Alberto Redondo <169093784+joaoalbertoredondo@users.noreply.github.com>
2025-02-21 10:34:40 -0300

    feat: setup prisma repositories

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,15 +3,15 @@ import { FetchRecentQuestionsController } from './controllers/fetch-recent-quest
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { AuthenticateController } from './controllers/authenticate.controller'
-import { PrismaService } from '../prisma/prisma.service'
+import { DatabaseModule } from '../database/database.module'
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [
     FetchRecentQuestionsController,
     CreateQuestionController,
     CreateAccountController,
     AuthenticateController,
   ],
-  providers: [PrismaService],
 })
 export class HttpModule {}
